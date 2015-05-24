ed4e924f3d5d20bec041a633c070b4c17c5248b5
Matt Lemmone <mattlemmone@att.net>
2015-05-24 18:43:23 -0400

    Added class functions that allow transposing and multiplying tempo of the song. These can be chained as seen in the script at the bottom. Added songs. Corrected timing on existing songs.

--- piezo.py
+++ piezo.py
@@ -2,37 +2,84 @@ import RPi.GPIO as GPIO
 import collections, time
 
 class Song:
-   'Store tempo, notes'
-   melody, timing = [], []
+    'Store tempo, notes'
+    melody, timing = [], []
 
-   def __init__(self, title):
+    def __init__(self, title):
       self.title = title
+
+    def transpose(self, halfSteps):
+        nbSz = len(noteBank) #notebank size
+        
+        for i, note in enumerate(self.melody): 
+            oldOct = note[-1] 
+            oldNote = note[:-1]
+            oldInd = noteBank.index(oldNote)
+            
+            newInd = oldInd + halfSteps            
+            newOct = int(oldOct) + newInd/nbSz
+              
+            newNote = noteBank[(newInd)%nbSz]
+            newNO = newNote + str(newOct) #new note + octave
+            
+            print oldNote + oldOct + '->' + newNO
+            
+            self.melody[i] = newNote + str(newOct) #transpose
+        return self
+
+    def tempoMult(self, multiplier):
+        nbSz = len(noteBank) #notebank size
+        
+        for i, t in enumerate(self.timing): 
+            oldT = t
+            newT = t / abs(multiplier)
+            
+            print str(oldT) + 's ->' + str(newT) +'s'
+            
+            self.timing[i] = newT
+            
+        return self
+             
+    def play(self):    
+        
+        time.sleep(1) #Prevent back-to-back chaining
+        GPIO.cleanup() #Can never be too safe!
     
-def playSong(Song):
-    GPIO.cleanup() #Can never be too safe!
-
-    print "Now Playing: " + Song.title
-
-    #GPIO prep
-    GPIO.setmode(GPIO.BOARD) #Because nobody likes .BCM
-    GPIO.setup(15, GPIO.OUT) #PWM pin = 15
-    p = GPIO.PWM(15, 90) #Set up pin
-    GPIO.output(15, True) #Start producing sound
-    p.start(90) #duty cycle @ 90%
-
-    for i in range(len(Song.melody)):  
-        note = noteFrequency[Song.melody[i]]
-        p.ChangeFrequency(note) 
-        print Song.melody[i] +" @ "+str(Song.timing[i]) + 's'
-        time.sleep(Song.timing[i])
-    
-    GPIO.cleanup()
-    print "Song Fin."
+        print "Now Playing: " + self.title
+        
+        #Check for unsupported notes before playing
+        for note in Song.melody:
+            try:
+                noteFrequency[note] #fix
+            except KeyError:
+                print "Unsupported note outside of octave range: " + note
+                print "Permitted range: " + noteBank[0]+str(baseOctave) +":" +noteBank[0]+str(loopSize-1)
+                print "Song exited."
+                return
+        
+        #GPIO prep
+        GPIO.setmode(GPIO.BOARD) #Because nobody likes .BCM
+        GPIO.setup(15, GPIO.OUT) #PWM pin = 15
+        p = GPIO.PWM(15, 90) #Set up pin
+        GPIO.output(15, True) #Start producing sound
+        p.start(80) #duty cycle @ 80%
+        
+        for i in range(len(self.melody)):  
+            note = self.melody[i]
+            freq = noteFrequency[note]
+            duration = self.timing[i]
+            
+            print note +" @ "+str(duration) + 's'
+            p.ChangeFrequency(freq) 
+            time.sleep(duration)
+        
+        GPIO.cleanup()
+        print "Song Fin."
 
 #Global vars (calm down, its just a script)
 noteFrequency = collections.OrderedDict() #note frequencies
-notesNotRoot = ["C", "C#","D","D#","E","F","F#","G", "G#", "A","A#","B"]
-baseOctave, octaveRange = 2, 4
+noteBank = ["C", "C#","D","D#","E","F","F#","G", "G#", "A","A#","B"]
+baseOctave, octaveRange = 2, 5
 loopSize = octaveRange + baseOctave + 1
 
 noteFrequency.update({"C2": 65.41}) #Lowest Note
@@ -40,15 +87,15 @@ noteFrequency.update({"C2": 65.41}) #Lowest Note
 #Instantiate dictionary of frequencies starting with C(baseOctave) -> C(loopSize)
 #Ex: baseOctave = 2, octaveRange = 4 -> C(2) -> C(6)
 for j in range(baseOctave, loopSize):
-    for i in range(0, len(notesNotRoot)):
+    for i in range(0, len(noteBank)):
         if (j == 2 and i == 0): continue #Skip C(baseOctave)
         if (j == loopSize - 1 and i == 1): break #Exit after C(loopSize)
         
         if i == 0: curOctave = j - 1 #When thisNote = C and examining the last note from the previous octave
         else: curOctave = j #thisNote != C, don't look at previous octave
         
-        lastNote = notesNotRoot[i-1] + str(curOctave)
-        thisNote = notesNotRoot[i] + str(j)
+        lastNote = noteBank[i-1] + str(curOctave)
+        thisNote = noteBank[i] + str(j)
         thisFreq = noteFrequency[lastNote] * pow(2, 1.0/12.0)
         
         noteFrequency.update({thisNote: thisFreq}) #update list
@@ -59,14 +106,16 @@ bubBob.melody = [
     "A4", "G#4", "F#4", 
     "E4", 
     "G#4", "F#4", "E4", "D4", 
-    "F#4", "E4", "D4", "C#4", "E4"]
-bubBob.timing = [
-    .3, .3, .3,
-    .3, .3, .4,
-    .2, 
-    .3, .3, .3, .4,
-    .3, .3, .2, .25, .5
+    "F#4", "E4", "D4", "C#4", "E4"
     ]
+bubBob.timing = [
+    .23, .23, .23,
+    .23, .23, .3,
+    .17, 
+    .23, .23, .2, .3,
+    .23, .23, .13, .17, .4
+    ]
+    
 rimShot = Song("I'm Not Funny")
 rimShot.melody = [
     "A4", "A4", "A4", 
@@ -80,6 +129,54 @@ rimShot.timing = [
     .55,
     .4, 
     ]  
-     
-playSong(bubBob)
-playSong(rimShot)
+    
+ff7 = Song("FF7 Victory")
+ff7.melody = [
+    "D4","D4","D4","D4",
+    "A#3","C4",
+    "D4","C4","D4",
+    ]
+ff7.timing = [
+    .18, .18, .18, .4,
+    .40, .40,
+    .30, .15, .30
+    ]  
+    
+zot = Song("Zelda - Owl Theme")
+zot.melody = [
+    "C4",
+    "B3", "C4",
+    "B3", "C4", "D4", "D#4", "F4", "G4", 
+    "C4", 
+    "G4", "C5",
+    "A#4", "G#4", "G4", "F4", 
+    "D#4", "D4", "C4", "B3", 
+    "G3",
+    
+    "C4",
+    "B3", "C4",
+    "B3", "C4", "D4", "D#4", "F4", "G4", 
+    "C4", 
+    "F4", "G#4", "G4", "F4", "D#4", "D4", "D4", "C4", "C4"
+]
+zot.timing = [
+    .35,
+    .16, .4,
+    .14, .14, .14, .14, .14, .2, 
+    .32,
+    .25, .25,
+    .17, .14, .14, .14,
+    .3, .14, .14, .3,
+    .35,
+    
+    .35,
+    .16, .4,
+    .16, .14, .14, .14, .14, .23, 
+    .33,
+    .25, .23, .14, .14, .14, .14, .4, .14, .3
+]
+zot.play()
+#bubBob.transpose(5).tempoMult(1.3).play()
+#rimShot.play()
+#ff7.transpose(3).play()
+#ff7.transpose(-1).tempoMult(1.6).play()
